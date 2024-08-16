7251a266937ba7ea02308d717c0d6327d1a55128
Maltonn <ymatsubayashi09@outlook.jp>
2024-08-16 15:25:02 +0900

    add excitation_number option (with SSVQE)

--- chemqulacs/vqe/vqemcscf.py
+++ chemqulacs/vqe/vqemcscf.py
@@ -14,6 +14,38 @@ from quri_parts.openfermion.transforms import jordan_wigner
 
 from chemqulacs.vqe.vqeci import VQECI, Ansatz, Backend, QulacsBackend
 
+def print_formatstring(energies,occ_indices_lst):
+    """
+    energies: list of float
+        list of energies of each state
+    occ_indices_lst: list of list of int
+        list of list of occupied indices of each state
+    print the formatted string
+    """
+    """
+    example:
+    
+    Calculated electronic
+    state   Energy(Hartree) Excitation energy(eV)   Occupancy indicates\n
+    S0      -74.964448      0.0000
+    S1      -74.567104      10.8123                 1 -> 2
+    S2      -74.528864      11.8528                 0 -> 2
+    S3      -74.528864      11.8528                 1 -> 3
+    """
+    base_energy=energies[0]
+    energy_diffs=[energy-base_energy for energy in energies]
+
+    base_occ_indices=occ_indices_lst[0]
+    occ_indices_diffs=[(set(base_occ_indices)-set(occ_indices),set(occ_indices)-set(base_occ_indices)) for occ_indices in occ_indices_lst]
+    occ_indices_diffs_str=[f"{','.join(map(str,occ_indices_diff[0]))} -> {','.join(map(str,occ_indices_diff[1]))}" if len(occ_indices_diff[0])>=1 else "" for occ_indices_diff in occ_indices_diffs]
+    states_str=["S"+str(i) for i in range(len(energies))]
+
+    print('Calculated electronic state')
+    print('state\tEnergy(Hartree)\tExcitation energy(eV)\tOccupancy indicates')
+    for i in range(len(energies)):
+        print(f"{states_str[i]}\t{energies[i]:7f}\t{energy_diffs[i]*27.2114:.4f}           \t{occ_indices_diffs_str[i]}")
+    
+
 
 class VQECASCI(casci.CASCI):
     """
@@ -68,6 +100,7 @@ class VQECASCI(casci.CASCI):
         layers: int = 1,
         k: int = 1,
         trotter_number: int = 1,
+        excitation_number:int=0,
         include_pi: bool = False,
         use_singles: bool = True,
         delta_sz: int = 0,
@@ -86,6 +119,7 @@ class VQECASCI(casci.CASCI):
             layers=layers,
             k=k,
             trotter_number=trotter_number,
+            excitation_number=excitation_number,
             include_pi=include_pi,
             use_singles=use_singles,
             delta_sz=delta_sz,
@@ -93,6 +127,9 @@ class VQECASCI(casci.CASCI):
             is_init_random=is_init_random,
             seed=seed,
         )
+
+    def print_energies(self):
+        print_formatstring(self.fcisolver.energies,self.fcisolver.occ_indices_lst)
 
 
 class VQECASSCF(mc1step.CASSCF):
@@ -149,6 +186,7 @@ class VQECASSCF(mc1step.CASSCF):
         layers: int = 1,
         k: int = 1,
         trotter_number: int = 1,
+        excitation_number:int=0,
         include_pi: bool = False,
         use_singles: bool = True,
         delta_sz: int = 0,
@@ -167,6 +205,7 @@ class VQECASSCF(mc1step.CASSCF):
             layers=layers,
             k=k,
             trotter_number=trotter_number,
+            excitation_number=excitation_number,
             include_pi=include_pi,
             use_singles=use_singles,
             delta_sz=delta_sz,
@@ -174,3 +213,6 @@ class VQECASSCF(mc1step.CASSCF):
             is_init_random=is_init_random,
             seed=seed,
         )
+
+    def print_energies(self):
+        print_formatstring(self.fcisolver.energies,self.fcisolver.occ_indices_lst)
